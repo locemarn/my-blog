bbd24eefd3fb11255e844d8d513291a85b9f4a3c
locemarn <marcelon27@gmail.com>
2019-09-01 22:05:46 -0300

    crate the first component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Profile = () => (
+    <>
+      <div className="Profile-wrapper">
+        <h1>Marcelo Nogueira</h1>
+        <h2>Full stack developer</h2>
+        <p>
+          Some words about me.
+        </p>
+      </div>
+    </>
+)
+
+export default Profile
