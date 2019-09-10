92316a255fb49dd85fe13bd247838eceba999219
locemarn <marcelon27@gmail.com>
2019-09-10 20:35:34 -0300

    social link component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/locemarn'
+  },
+  {
+    label: 'Twitter',
+    url: 'https:twitter.com/locemarn'
+  },
+  {
+    label: 'Youtube',
+    url: 'https://youtube.com/locemarn'
+  },
+  {
+    label: 'Instagram',
+    url: 'https://instagram.com/locemarn'
+  },
+  {
+    label: 'Unsplash',
+    url: 'https://unsplash.com/@locemarn'
+  },
+]
+
+export default Links
