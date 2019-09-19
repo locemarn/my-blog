101d0c6a7a56fefbe94bfb6bc9161e0e4cb94e99
locemarn <marcelon27@gmail.com>
2019-09-19 20:45:16 -0300

    structure data in PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,15 @@ import PostItem from '../components/PostItem'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+    <PostItem 
+      slug="/about/"
+      background="red"
+      category="Misc"
+      date="19 de Setembro de 2019"
+      timeToRead="5"
+      title="Post Title"
+      description="Post description"
+    />
   </Layout>
 )
 
